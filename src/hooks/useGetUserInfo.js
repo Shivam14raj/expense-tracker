823e808003dce4db6d746740f1b4409fab81e214
Shivam14raj <shivamrraj2005@gmail.com>
2026-02-23 00:19:00 +0530

    commit

--- src/hooks/useGetUserInfo.js
+++ src/hooks/useGetUserInfo.js
@@ -1,6 +1,25 @@
-export const useGetUserInfo = () =>{
-    const {name, profilePhoto, userID, isAuth} = JSON.parse(
-    localStorage.getItem('auth'))
+// export const useGetUserInfo = () =>{
+//     const {name, profilePhoto, userID, isAuth} = JSON.parse(
+//     localStorage.getItem('auth'))
 
-    return {name, profilePhoto, userID, isAuth}; // need seperatley these sometimes
-}
+//     return {name, profilePhoto, userID, isAuth}; // need seperatley these sometimes
+// }
+
+
+
+export const useGetUserInfo = () => {
+  const storedAuth = localStorage.getItem("auth");
+
+  if (!storedAuth) {
+    return {
+      name: null,
+      profilePhoto: null,
+      userID: null,
+      isAuth: false,
+    };
+  }
+
+  const { name, profilePhoto, userID, isAuth } = JSON.parse(storedAuth);
+
+  return { name, profilePhoto, userID, isAuth };
+};
